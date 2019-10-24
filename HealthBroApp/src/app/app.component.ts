f33b31a7a5ffab8ab2ec1bfd35a7b03b666b1f4e
Jacqueline Franßen <jacqueline.franssen@ibm.com>
2019-10-24 16:31:13 +0200

    added design and speechbubbles

--- HealthBroApp/src/app/app.component.ts
+++ HealthBroApp/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  title = 'MentalHealthBro';
+  title = 'HealthBro';
   speech_bubble = 'Check your health, Bro!';
 
   messages: any[];
