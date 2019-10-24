06cbff73c405452f35b6473acefc1c1a239f3bf2
Jacqueline Franßen <jacqueline.franssen@ibm.com>
2019-10-24 13:14:17 +0200

    added chat component

--- HealthBroApp/src/app/app.component.ts
+++ HealthBroApp/src/app/app.component.ts
@@ -1,11 +1,45 @@
 import { Component } from '@angular/core';
+// import { ChatShowcaseService } from './chat-showcase.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'MentalHealthBro';
   speech_bubble = 'Check your health, Bro!';
+
+  messages: any[];
+
+  // constructor(protected chatShowcaseService: ChatShowcaseService) {
+  //   this.messages = this.chatShowcaseService.loadMessages();
+  // }
+
+  sendMessage(event: any) {
+    const files = !event.files ? [] : event.files.map((file) => {
+      return {
+        url: file.src,
+        type: file.type,
+        icon: 'file-text-outline',
+      };
+    });
+
+    this.messages.push({
+      text: event.message,
+      date: new Date(),
+      reply: true,
+      type: files.length ? 'file' : 'text',
+      files: files,
+      user: {
+        name: 'Jonh Doe',
+        avatar: 'https://i.gifer.com/no.gif',
+      },
+    });
+    // const botReply = this.chatShowcaseService.reply(event.message);
+    // if (botReply) {
+    //   setTimeout(() => { this.messages.push(botReply) }, 500);
+    // }
+  }
 }
--- HealthBroApp/src/app/app.module.ts
+++ HealthBroApp/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule, NbSidebarModule, NbButtonModule } from '@nebular/theme';
+import { NbThemeModule, NbLayoutModule, NbSidebarModule, NbButtonModule, NbChatModule } from '@nebular/theme';
 
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 
@@ -22,6 +22,7 @@ import { NbEvaIconsModule } from '@nebular/eva-icons';
     NbLayoutModule,
     NbSidebarModule, // NbSidebarModule.forRoot(), //if this is your app.module
     NbButtonModule,
+    NbChatModule
   ],
   providers: [],
   bootstrap: [AppComponent]
